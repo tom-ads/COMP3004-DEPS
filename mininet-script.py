452ea1bad133d57da306843c1d799ec21b18ee53
tom-ads <thomas_adams@hotmail.co.uk>
2023-05-12 23:26:44 +0100

    feat: deps

---
+++ mininet-script.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+from mininet.net import Mininet
+from mininet.node import Controller, RemoteController, OVSController
+from mininet.node import CPULimitedHost, Host, Node
+from mininet.node import OVSKernelSwitch, UserSwitch
+from mininet.node import IVSSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+from mininet.link import TCLink, Intf
+from subprocess import call
+
+def myNetwork():
+    net = Mininet( topo=None,
+                   build=False,
+                   ipBase='10.0.0.0/8')
+
+    info( '*** Adding controller\n' )
+    c0=net.addController(name='c0',
+                      controller=RemoteController,
+                      ip='192.168.188.130',
+                      protocol='tcp',
+                      port=6633)
+
+    info( '*** Add switches\n')
+    s1 = net.addSwitch('s1', cls=OVSKernelSwitch)
+    s2 = net.addSwitch('s2', cls=OVSKernelSwitch)
+
+    info( '*** Add hosts\n')
+    h1 = net.addHost('h1', cls=Host, ip='10.0.0.1', defaultRoute=None)
+    h2 = net.addHost('h2', cls=Host, ip='10.0.0.2', defaultRoute=None)
+
+    info( '*** Add links\n')
+    s1s2 = {'bw':3.8}
+    net.addLink(s1, s2, cls=TCLink , **s1s2)
+    net.addLink(h1, s1)
+    net.addLink(s2, h2)
+
+    info( '*** Starting network\n')
+    net.build()
+    info( '*** Starting controllers\n')
+    for controller in net.controllers:
+        controller.start()
+
+    info( '*** Starting switches\n')
+    net.get('s1').start([c0])
+    net.get('s2').start([c0])
+
+    info( '*** Post configure switches and hosts\n')
+
+    CLI(net)
+
+myNetwork()
